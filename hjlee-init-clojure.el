e975d51b83663161cfc823ffc9f3bcdb22c08aa8
HJ Lee <hj.d.lee@gmail.com>
2008-09-29 19:14:21 +0900

    swank-clojure added.

--- hjlee-init-clojure.el
+++ hjlee-init-clojure.el
@@ -7,3 +7,11 @@
 (require 'clojure-auto)
 ;; or if you use paredit, uncomment the following
 (require 'clojure-paredit) 
+
+;;; swank-clojure
+
+(add-to-list 'load-path "~/usr/elisp/swank-clojure")
+
+(setq swank-clojure-jar-path (expand-file-name "~/usr/clojure/clojure.jar"))
+
+(require 'swank-clojure-autoload)

3d3c1642d4238b41034a51636f4c65db5f6fe2df
HJ Lee <hj.d.lee@gmail.com>
2008-11-21 17:09:12 +0900

    clojure.

--- hjlee-init-clojure.el
+++ hjlee-init-clojure.el
@@ -4,17 +4,23 @@
 
 
 (add-to-list 'load-path "~hjlee/usr/elisp/clojure-mode")
-(require 'clojure-auto)
-;; or if you use paredit, uncomment the following
-(require 'clojure-paredit) 
-
-;;; swank-clojure
-
 (add-to-list 'load-path "~hjlee/usr/elisp/slime")
 (add-to-list 'load-path "~hjlee/usr/elisp/swank-clojure")
 
-;; (setq swank-clojure-jar-path (expand-file-name "~hjlee/usr/clojure_git/clojure.jar"))
+(require 'clojure-auto)
+
+(require 'slime)
+(slime-setup)
 
 (setq swank-clojure-binary "clojure")
 
 (require 'swank-clojure-autoload)
+
+;; or if you use paredit, uncomment the following
+;; (require 'clojure-paredit)
+
+;;; swank-clojure
+
+
+;; (setq swank-clojure-jar-path (expand-file-name "~hjlee/usr/clojure_git/clojure.jar"))
+

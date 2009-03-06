31af1d0336b323986d92bcd26d290c0c86c74895
HJ Lee <hj.d.lee@gmail.com>
2009-03-06 09:27:41 +0900

    use copy of git-1.6.2/contrib/emacs.

--- hjlee-init-git.el
+++ hjlee-init-git.el
@@ -4,9 +4,7 @@
 ;;;   Author: Hwi-Jae. Lee. 
 ;;;
 
-(setq load-path (cons (expand-file-name "/usr/share/doc/git-core/contrib/emacs") load-path))
-(require 'vc-git)
-(when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
+(add-to-list 'load-path "~hjlee/usr/elisp/git-contrib-emacs")
 (require 'git)
 (autoload 'git-blame-mode "git-blame"
   "Minor mode for incremental blame for Git." t)

68ead7485620ba5cfd0882ed7ec9bea31a86ca98
HJ Lee <hj.d.lee@gmail.com>
2009-04-30 07:14:22 +0900

    enchant version.

--- hjlee-init-hunspell-ko.el
+++ hjlee-init-hunspell-ko.el
@@ -1,24 +1,25 @@
 ;;;
-;;;   $Id: elisp-insert.el,v 1.1 1996/12/10 12:25:00 hjlee Exp $
+;;;   
 ;;;
-;;;   Author: Hwi-Jae. Lee. 
+;;;   Author: Hwijae. Lee. 
 ;;;
 
-(setq ispell-program-name "hunspell") 
+(setq ispell-program-name "enchant") 
+
 (if (>= emacs-major-version 23)
     (setq ispell-local-dictionary-alist
 	  '(("korean"
 	     "[가-힣]"
 	     "[^가-힣]"
 	     "" nil
-	     ("-d" "/usr/share/myspell/dicts/ko_KR")
+	     ("-d" "ko_KR")
 	     nil utf-8)))
     (setq ispell-local-dictionary-alist
 	  '(("korean"
 	     "[가-힝]"
 	     "[^가-힝]"
 	     "" nil
-	     ("-d" "/usr/share/myspell/dicts/ko_KR")
+	     ("-d" "ko_KR")
 	     nil utf-8))))
 
 (setq ispell-dictionary "korean")

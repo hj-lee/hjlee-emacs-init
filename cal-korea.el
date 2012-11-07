26ddf4e4d6074a24e26b70369285077585453235
HJ Lee <hj.d.lee@gmail.com>
2012-11-08 01:34:09 +0900

    calendar-generate-month 재정의

--- cal-korea.el
+++ cal-korea.el
@@ -1,4 +1,4 @@
-;;; -*-mode: emacs-lisp; coding:euc-kr;-*-
+;;; -*-mode: emacs-lisp; coding:utf-8;-*-
 ;;;
 ;;;   $Id: cal-korea.el,v 1.1 2003/09/26 08:54:33 hjlee Exp $
 ;;;
@@ -13,10 +13,10 @@
     (year "/" month "/" day "[^0-9]")
     (month "\\." day "[^.0-9]")
     (year "\\." month "\\." day "[^0-9]")    
-    (month "¿ù *" day "ÀÏ" "[^.0-9]")
-;    (month "¿ù *" day "[^.0-9]")
-    (year "³â *" month "¿ù *" day "ÀÏ" "[^0-9]")    
-;    (year "³â *" month "¿ù *" day "[^0-9]")    
+    (month "ì›” *" day "ì¼" "[^.0-9]")
+;    (month "ì›” *" day "[^.0-9]")
+    (year "ë…„ *" month "ì›” *" day "ì¼" "[^0-9]")    
+;    (year "ë…„ *" month "ì›” *" day "[^0-9]")    
     (month " *" day "[^0-9]")
     (month "," day "[^0-9]")
     (dayname "\\W"))
@@ -25,260 +25,25 @@ See the documentation of `diary-date-forms' for an explanation.")
 
 ;;;###autoload
 (defvar korean-calendar-display-form
-  '(year "³â " month "¿ù " day "ÀÏ" 
+  '(year "ë…„ " month "ì›” " day "ì¼" 
     (if dayname (concat " " dayname))))
 
-(setq calendar-day-name-array
-      ["ÀÏ¿äÀÏ" "¿ù¿äÀÏ" "È­¿äÀÏ" "¼ö¿äÀÏ" "¸ñ¿äÀÏ" "±İ¿äÀÏ" "Åä¿äÀÏ"])
+(defvar korean-calendar-day-name-array
+      ["ì¼ìš”ì¼" "ì›”ìš”ì¼" "í™”ìš”ì¼" "ìˆ˜ìš”ì¼" "ëª©ìš”ì¼" "ê¸ˆìš”ì¼" "í† ìš”ì¼"])
 
-(setq calendar-month-name-array
-      ["1¿ù " "2¿ù " "3¿ù " "4¿ù " "5¿ù " "6¿ù " "7¿ù " "8¿ù " "9¿ù " "10¿ù" "11¿ù" 
-       "12¿ù"])
+(defvar korean-calendar-month-name-array
+      ["1ì›” " "2ì›” " "3ì›” " "4ì›” " "5ì›” " "6ì›” " "7ì›” " "8ì›” " "9ì›” " "10ì›”" "11ì›”" 
+       "12ì›”"])
 
 (defun korean-calendar ()
   "Set the interpretation and display of dates to the Korean style."
   (interactive)
+  (setq calendar-day-name-array korean-calendar-day-name-array)
+  (setq calendar-month-name-array korean-calendar-month-name-array)
+
   (setq european-calendar-style nil)
   (setq calendar-date-display-form korean-calendar-display-form)
   (setq diary-date-forms korean-date-diary-pattern)
-  ;; (update-calendar-mode-line)
 )
-
-;;;
-;;; re-defun calendar-date-string
-;;;     change abbreviate dayname size from 3 to 2.
-;;;
-
-; (defun calendar-date-string (date &optional abbreviate nodayname)
-;   "A string form of DATE, driven by the variable `calendar-date-display-form'.
-; An optional parameter ABBREVIATE, when t, causes the month and day names to be
-; abbreviated to three characters.  An optional parameter NODAYNAME, when t,
-; omits the name of the day of the week."
-;   (let* ((dayname
-;           (if nodayname
-;               nil
-;             (if abbreviate
-;                 (substring (calendar-day-name date) 0 2)
-;               (calendar-day-name date))))
-;          (month (extract-calendar-month date))
-;          (monthname
-;           (if abbreviate
-;               (substring
-;                (calendar-month-name month) 0 3)
-;             (calendar-month-name month)))
-;          (day (int-to-string (extract-calendar-day date)))
-;          (month (int-to-string month))
-;          (year (int-to-string (extract-calendar-year date))))
-;     (mapconcat 'eval calendar-date-display-form "")))
-
-(defun calendar-date-string (date &optional abbreviate nodayname)
-  "A string form of DATE, driven by the variable `calendar-date-display-form'.
-An optional parameter ABBREVIATE, when t, causes the month and day names to be
-abbreviated to three characters.  An optional parameter NODAYNAME, when t,
-omits the name of the day of the week."
-  (let* ((dayname
-          (if nodayname
-              nil
-            (if abbreviate
-                (calendar-day-name date 1)
-              (calendar-day-name date))))
-         (month (extract-calendar-month date))
-         (monthname
-          (if abbreviate
-              (calendar-month-name month 2)
-            (calendar-month-name month)))
-         (day (int-to-string (extract-calendar-day date)))
-         (month (int-to-string month))
-         (year (int-to-string (extract-calendar-year date))))
-    (mapconcat 'eval calendar-date-display-form "")))
-
-;;;
-;;; re-defun generate-calendar-month 
-;;;          change month line.
-;;;
-
-(defun generate-calendar-month (month year indent)
-  "Produce a calendar for MONTH, YEAR on the Gregorian calendar.
-The calendar is inserted in the buffer starting at the line on which point
-is currently located, but indented INDENT spaces.  The indentation is done
-from the first character on the line and does not disturb the first INDENT
-characters on the line."
-  (let* ((blank-days;; at start of month
-          (mod
-           (- (calendar-day-of-week (list month 1 year))
-              calendar-week-start-day)
-           7))
-	 (last (calendar-last-day-of-month month year)))
-   (goto-char (point-min))
-   (calendar-insert-indented
-    (calendar-string-spread
-     (list (format "%d³â %s" year (calendar-month-name month))) ?  20)
-    indent t)
-   (calendar-insert-indented "" indent);; Go to proper spot
-   (calendar-for-loop i from 0 to 6 do
-      (insert (substring (aref calendar-day-name-array 
-                               (mod (+ calendar-week-start-day i) 7))
-                         0 1))
-      (insert " "))
-   (calendar-insert-indented "" 0 t);; Force onto following line
-   (calendar-insert-indented "" indent);; Go to proper spot
-   ;; Add blank days before the first of the month
-   (calendar-for-loop i from 1 to blank-days do (insert "   "))
-   ;; Put in the days of the month
-   (calendar-for-loop i from 1 to last do
-      (insert (format "%2d " i))
-      (put-text-property (- (point) 3) (1- (point))
-			 'mouse-face 'highlight)
-      (and (zerop (mod (+ i blank-days) 7))
-           (/= i last)
-           (calendar-insert-indented "" 0 t)    ;; Force onto following line
-           (calendar-insert-indented "" indent)))));; Go to proper spot
-
-;;;
-;;; re-defun fts in diary-lib
-;;;
-
-(require 'diary-lib)
-
-(defun insert-yearly-diary-entry (arg)
-  "Insert an annual diary entry for the day of the year indicated by point.
-Prefix arg will make the entry nonmarking."
-  (interactive "P")
-  (let* ((calendar-date-display-form
-	  '(monthname " " day "ÀÏ")))
-    (make-diary-entry (calendar-date-string (calendar-cursor-to-date t) t)
-                      arg)))
-
-
-;;;
-;;; modification for chinese-date
-;;;
-
-(require 'cal-china)
-
-;; (defun calendar-chinese-date-string (&optional date)
-;;   "String of Chinese date of Gregorian DATE.
-;; Defaults to today's date if DATE is not given."
-;;   (let* ((a-date (calendar-absolute-from-gregorian
-;; 		  (or date (calendar-current-date))))
-;; 	 (c-date (calendar-chinese-from-absolute a-date))
-;; 	 (cycle (car c-date))
-;; 	 (year (car (cdr c-date)))
-;; 	 (month (car (cdr (cdr c-date))))
-;; 	 (day (car (cdr (cdr (cdr c-date)))))
-;; 	 (this-month (calendar-absolute-from-chinese
-;; 		      (list cycle year month 1)))
-;; 	 (next-month (calendar-absolute-from-chinese
-;; 		      (list (if (= year 60) (1+ cycle) cycle)
-;; 			    (if (= (floor month) 12) (1+ year) year)
-;; 			    (calendar-mod (1+ (floor month)) 12)
-;; 			    1)))
-;; 	 (m-cycle (% (+ (* year 5) (floor month)) 60)))
-;;     (format "%s %s.%s"
-;; 	    (if (not (integerp month))
-;; 		"second "
-;; 	      (if (< 30 (- next-month this-month))
-;; 		  "first "
-;; 		""))
-;; 	    (floor month)
-;; 	    day )))
-
-;; (defun diary-chinese-date ()
-;;   "Chinese calendar equivalent of date diary entry."
-;;   (format "(-)%s" (calendar-chinese-date-string date)))
-      
-;;;
-;;; modification for cal-tex
-;;;
-(require 'cal-tex)
-
-(defun cal-tex-list-diary-entries (d1 d2)
-  "Generate a list of all diary-entries from absolute date D1 to D2."
-  (let ((diary-display-hook nil))
-    (reverse 
-     (list-diary-entries
-      (calendar-gregorian-from-absolute d1)
-      (1+ (- d2 d1))))))
-
-(setq cal-tex-cal-one-month
-      "\\def\\calmonth#1#2%
-{\\begin{center}%
-\\Huge\\bf #2³â #1 \\\\[1cm]%
-\\end{center}}% 
-\\vspace*{-1.5cm}%
-%
-")
-
-
-(defun cal-tex-preamble (&optional args)
-  "Insert the LaTeX preamble.
-Preamble Includes initial definitions for various LaTeX commands.
-Optional ARGS are included."
-  (set-buffer (get-buffer-create cal-tex-buffer))
-  (erase-buffer)
-  (insert "\\documentclass")
-  (if args
-      (insert "[" args "]"))
-  (insert "{article}\n"
-	  "\\usepackage{hangul}
-\\hbadness 20000
-\\hfuzz=1000pt
-\\vbadness 20000
-\\marginparwidth 0pt
-\\oddsidemargin  -2.2cm
-\\evensidemargin -2.2cm
-\\marginparsep   0pt
-\\topmargin      0pt
-\\textwidth      7.5in
-\\textheight     9.5in
-\\newlength{\\cellwidth}
-\\newlength{\\cellheight}
-\\newlength{\\boxwidth}
-\\newlength{\\boxheight}
-\\newlength{\\cellsize}
-\\newcommand{\\myday}[1]{}
-\\newcommand{\\caldate}[6]{}
-\\newcommand{\\nocaldate}[6]{}
-\\newcommand{\\calsmall}[6]{}
-%
-"
-	  ))
-
-(defun cal-tex-insert-preamble (weeks landscape size &optional append)
-  "Initialize the output buffer.
-Select the output buffer, and insert the preamble for a calendar of 
-WEEKS weeks.  Insert code for landscape mode if LANDSCAPE is true. 
-Use pointsize SIZE.  Optional argument APPEND, if t, means add to end of
-without erasing current contents."
-  (let ((width "18cm")
-	(height "24cm"))
-    (if landscape
-	(progn
-	  (setq width "24cm")
-	  (setq height "18cm")))
-    (if (not append)
-	(progn
-	  (cal-tex-preamble size)
-	  (if (not landscape) 
-	      (progn
-		(cal-tex-cmd "\\oddsidemargin -1.95cm")
-		(cal-tex-cmd "\\def\\holidaymult{.06}"))
-	    (cal-tex-cmd "\\special{landscape}")
-	    (cal-tex-cmd "\\textwidth  9.5in")
-	    (cal-tex-cmd "\\textheight 7in")
-	    (cal-tex-comment)
-	    (cal-tex-cmd "\\def\\holidaymult{.08}"))
-	  (cal-tex-cmd  cal-tex-caldate)
-	  (cal-tex-cmd  cal-tex-myday)
-	  (cal-tex-b-document)
-	  (cal-tex-cmd "\\pagestyle{empty}")))
-    (cal-tex-cmd "\\setlength{\\cellwidth}" width)
-    (insert (format "\\setlength{\\cellwidth}{%f\\cellwidth}\n"
-		    (/ 1.1 (length cal-tex-which-days))))
-    (cal-tex-cmd "\\setlength{\\cellheight}" height)
-    (insert (format "\\setlength{\\cellheight}{%f\\cellheight}\n"
-		    (/ 1.0 weeks)))              
-    (cal-tex-cmd "\\ \\par")
-    (cal-tex-vspace "-3cm")))
 
 (provide 'cal-korea)
--- hjlee-init-calendar.el
+++ hjlee-init-calendar.el
@@ -44,6 +44,8 @@
 
 (korean-calendar)
 
+
+
 ;;;;;;;;;;;;;;;;;;;;
 ;;;;
 ; diary
@@ -53,3 +55,94 @@
 ;(diary 0)
 
 
+(defface sunday
+  '((((class color) (background light))
+     :background "#ff3a3a")
+    (((class color) (background dark))
+     :background "red")
+    (t
+     :inverse-video t))
+  "Face for indicating in the calendar dates that have holidays.
+See `calendar-holiday-marker'."
+  :group 'calendar-faces)
+
+
+(defface saturday
+  '((((class color) (background light))
+     :background "cyan")
+    (((class color) (background dark))
+     :background "blue")
+    (t
+     :inverse-video t))
+  "Face for indicating in the calendar dates that have holidays.
+See `calendar-holiday-marker'."
+  :group 'calendar-faces)
+
+
+
+(defun calendar-generate-month (month year indent)
+  "Produce a calendar for MONTH, YEAR on the Gregorian calendar.
+The calendar is inserted at the top of the buffer in which point is currently
+located, but indented INDENT spaces.  The indentation is done from the first
+character on the line and does not disturb the first INDENT characters on the
+line."
+  (let ((blank-days                     ; at start of month
+         (mod
+          (- (calendar-day-of-week (list month 1 year))
+             calendar-week-start-day)
+          7))
+         (last (calendar-last-day-of-month month year))
+         (trunc (min calendar-intermonth-spacing
+                     (1- calendar-left-margin)))
+         (day 1)
+         string)
+   (goto-char (point-min))
+   (calendar-move-to-column indent)
+   (insert
+    (calendar-string-spread
+     (list (format "%d년 %s" year (calendar-month-name month)))
+     ?\s calendar-month-digit-width))
+   (calendar-ensure-newline)
+   (calendar-insert-at-column indent calendar-intermonth-header trunc)
+   ;; Use the first two characters of each day to head the columns.
+   (dotimes (i 7)
+     (insert
+      (progn
+        (setq string
+              (calendar-day-name (mod (+ calendar-week-start-day i) 7) nil t))
+        (if enable-multibyte-characters
+            (truncate-string-to-width string calendar-day-header-width)
+          (substring string 0 calendar-day-header-width)))
+      (make-string (- calendar-column-width calendar-day-header-width) ?\s)))
+   (calendar-ensure-newline)
+   (calendar-insert-at-column indent calendar-intermonth-text trunc)
+   ;; Add blank days before the first of the month.
+   (insert (make-string (* blank-days calendar-column-width) ?\s))
+   ;; Put in the days of the month.
+   (dotimes (i last)
+     (setq day (1+ i))
+     ;; TODO should numbers be left-justified, centered...?
+     (insert (format (format "%%%dd%%s" calendar-day-digit-width) day
+                     (make-string
+                      (- calendar-column-width calendar-day-digit-width) ?\s)))
+     ;; 'date property prevents intermonth text confusing re-searches.
+     ;; (Tried intangible, it did not really work.)
+     (set-text-properties
+      (- (point) (1+ calendar-day-digit-width)) (1- (point))
+      `(mouse-face highlight help-echo ,(eval calendar-date-echo-text)
+                   date t))
+
+     (when (= 1 (mod (+ day blank-days) 7))
+       (overlay-put
+	(make-overlay (- (point) 3) (- (point) 1)) 'face 'sunday))
+
+     (when (zerop (mod (+ day blank-days) 7))
+       (overlay-put
+	(make-overlay (- (point) 3) (- (point) 1)) 'face 'saturday))
+
+
+     (when (and (zerop (mod (+ day blank-days) 7))
+                (/= day last))
+       (calendar-ensure-newline)
+       (setq day (1+ day))              ; first day of next week
+       (calendar-insert-at-column indent calendar-intermonth-text trunc)))))

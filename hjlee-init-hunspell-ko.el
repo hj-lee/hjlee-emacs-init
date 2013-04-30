e6de916d857f8987b7a3314a67beb20d14e1c68b
HJ Lee <hj.d.lee@gmail.com>
2013-04-30 18:41:12 +0900

    Fix a mistake.

--- hjlee-init-hunspell-ko.el
+++ hjlee-init-hunspell-ko.el
@@ -10,19 +10,19 @@
 ;; 사전 목록에 한국어("korean") 추가
 (if (>= emacs-major-version 23)
     (setq ispell-local-dictionary-alist
-          '("korean"
-            "[가-힣]"
-            "[^가-힣]"
-            "[0-9a-zA-Z]" nil
-            ("-d" "ko_KR")
-            nil utf-8))
-    (setq  ispell-local-dictionary-alist
-           '("korean"
-             "[가-힝]"
-             "[^가-힝]"
-             "[0-9a-zA-Z)]" nil
+          '(("korean"
+             "[가-힣]"
+             "[^가-힣]"
+             "[0-9a-zA-Z]" nil
              ("-d" "ko_KR")
              nil utf-8)))
+  (setq  ispell-local-dictionary-alist
+         '(("korean"
+            "[가-힝]"
+            "[^가-힝]"
+            "[0-9a-zA-Z)]" nil
+            ("-d" "ko_KR")
+            nil utf-8))))
 
 ;; 한국어를 기본 사전으로 지정
 (setq ispell-dictionary "korean")

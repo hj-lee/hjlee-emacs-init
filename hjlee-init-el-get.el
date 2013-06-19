768dacf61a530ed19fc4fe8e640aba230a7f5f9d
HJ Lee <hj.d.lee@gmail.com>
2013-06-20 08:41:17 +0900

    Shamelessly copy rcps from master el-get.

--- hjlee-init-el-get.el
+++ hjlee-init-el-get.el
@@ -6,12 +6,23 @@
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
+
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
+
+;; (setq el-get-recipe-path-emacswiki
+;;       "~/.emacs.d/el-get/el-get/recipes/emacswiki/")
+
+;; ;; (push "~/.emacs.d/el-get-dev/recipes" el-get-recipe-path)
+;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-dev/recipes")
+(add-to-list 'el-get-recipe-path "~/hjlee-emacs-init/el-get-recipes")
+
+;;
+
 
 ;; leave slime to be managed by quicklisp
 (setq my:el-get-packages
@@ -29,7 +40,8 @@
         rope
         ropemacs
         ropemode
-        scala-mode
+        scala-mode2
+        ensime
         ))
 
 (el-get 'sync my:el-get-packages)

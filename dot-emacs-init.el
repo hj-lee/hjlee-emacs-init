b7cc5e978fc739048194fb0a4808d400150f990a
Hwijae Lee <Hwijae.Lee@soti.net>
2017-12-27 13:46:18 -0500

    Commented out Fira Code

--- dot-emacs-init.el
+++ dot-emacs-init.el
@@ -41,6 +41,8 @@
 (load-library "hjlee-init-helm")
 (load-library "hjlee-init-helm-gtags")
 
+;; (load-library "hjlee-init-firacode")
+
 ;;;; chromium browser edit plug-in
 (require 'edit-server)
 (edit-server-start)

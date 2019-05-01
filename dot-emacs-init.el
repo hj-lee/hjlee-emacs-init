6cb822d1d66c52a89f3edb92bfe3fecc4e06a502
Hwijae Lee <hj.d.lee@gmail.com>
2019-04-30 20:04:54 -0400

    Remove gnuserv init

--- dot-emacs-init.el
+++ dot-emacs-init.el
@@ -49,23 +49,6 @@
 ;; ;; (setq ispell-program-name "enchant") 
 ;; ;; (setq ispell-program-name "ispell") 
 
-;; for experiment with emacs-cvs
-(let ((path "/usr/share/emacs/site-lisp/gnuserv"))
-  (unless (find path load-path :test 'equal)
-    (add-to-list 'load-path path)
-    (autoload (quote gnuserv-start) "gnuserv-compat"
-    "Allow this Emacs process to be a server for client processes.
-This starts a gnuserv communications subprocess through which
-client \"editors\" (gnuclient and gnudoit) can send editing commands to
-this Emacs job.  See the gnuserv(1) manual page for more details.
-
-Prefix arg means just kill any existing server communications subprocess."
-
-    t nil)))
-
-
-(setq gnuserv-frame t)
-(gnuserv-start)
 
 ;;;;;;;;;;;;;
 ;;; emacs customize setting

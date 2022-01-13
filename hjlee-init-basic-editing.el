0c5a3029f4c20d37dd9eff0ff926e5c874533f3a
Hwijae Lee <hj.d.lee@gmail.com>
2022-01-12 19:15:25 -0500

    Use setq instead of customize for several variables

--- hjlee-init-basic-editing.el
+++ hjlee-init-basic-editing.el
@@ -10,3 +10,18 @@
 
 (add-to-list 'auto-mode-alist 
 	     '("\\.h$" . c++-mode))
+
+(setq column-number-mode t)
+(setq case-fold-search nil)
+(setq show-paren-mode t)
+(setq tool-bar-mode nil)
+(setq vc-follow-symlinks t)
+
+(setq version-control t)
+(setq delete-old-versions t)
+(setq kept-old-versions 2)
+(setq kept-new-versions 3)
+
+;; (setq ido-default-buffer-method 'selected-window)
+;; (setq ido-everywhere t)
+

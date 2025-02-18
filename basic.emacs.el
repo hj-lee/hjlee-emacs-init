6c66128b59ab78bc9acdf6762b37d0b72b8204a0
Hwijae Lee <hj.d.lee@gmail.com>
2025-02-18 00:20:31 -0500

    Remove user-init-file and user-emacs-directory override

--- basic.emacs.el
+++ basic.emacs.el
@@ -4,8 +4,5 @@
 ;; ln -s ~/hjlee-emacs-init/basic.emacs.el ~/.emacs.el
 ;;
 
-(setq user-init-file (or load-file-name (buffer-file-name)))
-(setq user-emacs-directory (file-name-directory user-init-file))
-
 (setq load-path (cons "~/hjlee-emacs-init" load-path))
 (load-library "dot-emacs-init.el")
